25c64448dd71a2ab5c11c276745d4f69a3b962a9
Etienne Lemonnier <56196021+elemonnier@users.noreply.github.com>
2021-11-11 14:32:34 +0100

    Add files via upload

---
+++ TP5/page.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+echo "ID : ".session_id()."<br>";
+echo "Bonjour ".$_SESSION['nom']." (".$_SESSION['mail'].")<br>";
+echo "Ta première connexion était le ".$_SESSION['date']."<br>";
+?>
+
+<h2>test2</h2>
+
+<a href="<?php echo $_SESSION['site']; ?>">Cliquer pour ouvrir votre site préféré</a>
